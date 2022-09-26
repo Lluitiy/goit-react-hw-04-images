e925061bf67cdcc78507e8726d84ed8a1084d622
Pavlo Vovzhynskyy <pavlovovzhynskyy@gmail.com>
2022-09-26 02:46:30 +0200

    comment never used values yet for linter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,8 +4,8 @@ import { Container } from './App.styled';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Spinner } from 'components/Spinner/Spinner';
 import { LoadMoreBtn } from 'components/LoadMoreBtn/LoadMoreBtn';
-import { Modal } from '../Modal/Modal';
-import { ToastContainer, toast } from 'react-toastify';
+// import { Modal } from '../Modal/Modal';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export class App extends Component {
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { Overlay, ModalContent } from './Modal.styled';
-const modalRoot = document.querySelector('#modal-root');
+// const modalRoot = document.querySelector('#modal-root');
 
 export class Modal extends Component {
 	componentDidMount() {}
